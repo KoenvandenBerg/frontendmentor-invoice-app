06b8013845d02c396af6a9b10b8d8e68678cc078
Koen van den Berg <git@koenvandenberg.dev>
2023-09-18 09:21:14 +0200

    Resolve build error

--- src/components/invoices-overview/InvoicesOverviewList.tsx
+++ src/components/invoices-overview/InvoicesOverviewList.tsx
@@ -2,10 +2,12 @@ import { FilterState } from '@/hooks/useInvoicesFilter';
 import { Invoice } from '@/redux/features/invoicesSlice';
 import InvoicesOverviewListItem from './InvoicesOverviewListItem';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Dispatch } from 'react';
 
 type InvoicesOverviewListProps = {
   filters: FilterState;
   invoices: Invoice[];
+  setOpenInvoice: Dispatch<Invoice | null>;
 };
 
 function getInvoicesToRender(invoices: Invoice[], filters: FilterState) {
@@ -26,22 +28,33 @@ function getInvoicesToRender(invoices: Invoice[], filters: FilterState) {
 export default function InvoicesOverviewList({
   filters,
   invoices,
+  setOpenInvoice,
 }: InvoicesOverviewListProps) {
   return (
-    <motion.div layout>
-      <AnimatePresence initial={false}>
-        {invoices.length > 0 ? (
-          getInvoicesToRender(invoices, filters).map((invoice, index) =>
-            index === 0 ? (
-              <InvoicesOverviewListItem invoice={invoice} key={invoice.id} />
-            ) : (
-              <InvoicesOverviewListItem invoice={invoice} key={invoice.id} />
+    <>
+      <motion.div layout>
+        <AnimatePresence initial={false}>
+          {invoices.length > 0 ? (
+            getInvoicesToRender(invoices, filters).map((invoice, index) =>
+              index === 0 ? (
+                <InvoicesOverviewListItem
+                  invoice={invoice}
+                  key={invoice.id}
+                  setOpenInvoice={() => setOpenInvoice(invoice)}
+                />
+              ) : (
+                <InvoicesOverviewListItem
+                  invoice={invoice}
+                  key={invoice.id}
+                  setOpenInvoice={() => setOpenInvoice(invoice)}
+                />
+              )
             )
-          )
-        ) : (
-          <p>No Invoices</p>
-        )}
-      </AnimatePresence>
-    </motion.div>
+          ) : (
+            <p>No Invoices</p>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </>
   );
 }
